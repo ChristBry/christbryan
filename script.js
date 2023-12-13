83b2ce094c084779b88accebafc1208542502d68
Christbryan <tchajipchrist23@gmail.com>
2023-12-14 00:53:55 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+/** ========================== typing animatio======= */
+var typed = new Typed(".typing",{
+    strings:["IT Manager","Information System manager","Network Administration","Business Intelligent","Project Manager","IT Aviation"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
