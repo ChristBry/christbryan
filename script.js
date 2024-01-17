f2112580955cbf27f9d8c86488da68831ea143fb
ChristBry <153141007+ChristBry@users.noreply.github.com>
2024-01-17 23:58:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /** ========================== typing animatio======= */
 var typed = new Typed(".typing",{
-    strings:["IT Manager","Information System manager","Network Administration","Business Intelligent","Project Manager","IT Aviation"],
+    strings:["DevOps","IT Manager","Network Administration","Business Intelligent","Product Manager","IT Aviation"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
